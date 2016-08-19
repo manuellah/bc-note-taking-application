337c40adf76a1f54b9d6ead36e9cd0e61c222395
manuellah <emmanumuthui@gmail.com>
2016-08-19 14:59:45 +0300

    Added some changes

--- NotesApplication.py
+++ NotesApplication.py
@@ -1,13 +1,14 @@
 class NotesApplication(object):
     notes_id=0
+    note_list=[]
     def __init__(self,author="Author"):
         self.author=author
-        self.note_list=[]
         NotesApplication.notes_id+=1
         self.id=NotesApplication.notes_id
         
-    def create(self, note_content=input("What do you have in mind!!!!???")):
-        self.note_list.append(note_content)
+    def create(self, note_content=""):
+        note_content=input("What do you have in mind!!!!???  :   ")
+        NotesApplication.note_list.append(note_content)
     
     def list(self):
         for item in self.note_list:
@@ -15,7 +16,7 @@ class NotesApplication(object):
             print(item)
             print()
             print("By Author","  ", self.author)
-            
+
     
     def get(self, note_id):
         return str(self.note_list[note_id])
@@ -33,13 +34,14 @@ class NotesApplication(object):
     def delete(self, note_id):
         self.note_list.pop(note_id)
     
-    def edit(self, note_id, new_content = input("Write you edit text here")):
+    def edit(self, note_id, new_content):
+        new_content = input("Write you edit text here   :")
         self.note_list[note_id]=new_content
     
 s=NotesApplication("Emmanuel")
 s.create()
 s.list()
-s.get(0)
-s.search("qusai")
-#s.edit(0)
+print(s.get(0))
+
         
+    
