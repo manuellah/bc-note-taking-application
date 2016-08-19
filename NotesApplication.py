89640daec8aed6d980c27c2b279f571f0457ca73
manuellah <emmanumuthui@gmail.com>
2016-08-19 11:19:18 +0300

    half way done

--- NotesApplication.py
+++ NotesApplication.py
@@ -1,19 +1,36 @@
 class NotesApplication(object):
+    notes_id=0
     def __init__(self,author="Author"):
         self.author=author
         self.note_list=[]
+        NotesApplication.notes_id+=1
+        self.id=NotesApplication.notes_id
         
     def create(self, note_content):
-        pass
+        self.note_list.append(note_content)
     
     def list(self):
-        pass
+        for item in self.note_list:
+            print("Note ID","  ",self.id)
+            print(item)
+            print()
+            print("By Author","  ", self.author)
+            
     
     def get(self, note_id):
-        pass
+        return str(self.note_list[note_id])
     
     def search(self, search_text):
-        pass
+        notes_index_list=[]
+        print("Showing results for search ", search_text)
+        for item in self.note_list:
+            item=str(item)
+            if search_text in item:
+                print
+                self.list()
+                print("==============================================")
+                
+            
     
     def delete(self, note_id):
         pass
